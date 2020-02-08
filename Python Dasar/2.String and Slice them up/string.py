b828ee072fec08a7cfd2484b63fd76367703afd7
torangsitungkir93 <torangsitungkir93@gmail.com>
2020-02-08 17:48:24 +0700

    First Commit

---
+++ Python Dasar/2.String and Slice them up/string.py
@@ -0,0 +1,8 @@
+text1 = "Jalan jalan kuyyy"
+text2 = r"C:\nAhsiappp"
+
+print(text1)
+print(text2)
+print(5*"wk")
+
+print('kue' 'pukis')
---
+++ Python Dasar/2.String and Slice them up/string2.py
@@ -0,0 +1,6 @@
+text = "Pisang goreng"
+
+a=text[0:5]
+b=text[-4]
+print (a)
+print (b)
